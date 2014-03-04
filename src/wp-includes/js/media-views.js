e474a1b3b7ca7f00448664fd163879b1b67102d0
Gregory Cornelius <gcorne@gmail.com>
2014-03-04 16:56:08 -0500

    More whitespace cleanup.

--- src/wp-includes/js/media-views.js
+++ src/wp-includes/js/media-views.js
@@ -1511,11 +1511,11 @@
 			if ( canSkipCrop ) {
 				_.extend( toolbarOptions.items, {
 					skip: {
-						style:		'primary',
-						text:	l10n.skipCropping,
-						priority:	70,
-						requires:	{ library: false, selection: false },
-						click:		function() {
+						style:      'secondary',
+						text:       l10n.skipCropping,
+						priority:   70,
+						requires:   { library: false, selection: false },
+						click:      function() {
 							var selection = this.controller.state().get('selection').first();
 							this.controller.state().cropperView.remove();
 							this.controller.trigger('skippedcrop', selection);
